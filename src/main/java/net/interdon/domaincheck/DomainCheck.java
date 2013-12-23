a17f9796d47f9eaabac11b38a21a787e2135cb92
Evgeniy Dolgikh <kasarch@atsy.org.ua>
2013-12-23 20:43:52 +0200

    add queryng

--- src/main/java/net/interdon/domaincheck/DomainCheck.java
+++ src/main/java/net/interdon/domaincheck/DomainCheck.java
@@ -5,14 +5,33 @@
 
 package net.interdon.domaincheck;
 
+import net.interdon.domaincheck.containers.Domain;
+import org.apache.commons.net.whois.WhoisClient;
+
+import java.io.IOException;
 import java.util.List;
 
 public class DomainCheck {
     private IServersPool servers;
     private List domains;
+    private int currentDomain = 0;
+    WhoisClient whoisClient;
 
     public DomainCheck(IServersPool servers, List domains) {
         this.servers = servers;
         this.domains = domains;
+        whoisClient = new WhoisClient();
     }
+
+    public Domain query(Domain domain) {
+        try {
+            whoisClient.connect(servers.nextServer());
+            domain.setWhoisResponce(whoisClient.query(domain.getDomainName()));
+            whoisClient.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return domain;
+    }
+
 }
