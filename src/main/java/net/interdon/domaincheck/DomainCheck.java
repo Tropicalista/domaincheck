c527aee90e80ddf1824108b69365ea72dc1f70d5
Evgeniy Dolgikh <marcon@atsy.org.ua>
2013-12-23 14:48:24 +0200

    add main class

---
+++ src/main/java/net/interdon/domaincheck/DomainCheck.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2013 Evgeniy Dolgikh <marcon@atsy.org.ua>
+ * See the file LICENSE for copying permission.
+ */
+
+package net.interdon.domaincheck;
+
+public class DomainCheck {
+    public static void main(String[] args) {
+
+    }
+}
