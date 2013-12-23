1bb589972e97314fc90e25435def9f36446dcd8f
Evgeniy Dolgikh <kasarch@atsy.org.ua>
2013-12-23 18:05:55 +0200

    Basic serverspool implementation

--- src/main/java/net/interdon/domaincheck/ServersPool.java
+++ src/main/java/net/interdon/domaincheck/ServersPool.java
@@ -5,9 +5,16 @@
 
 package net.interdon.domaincheck;
 
-public class ServersPool implements IServersPool {
-    public ServersPool(String[] servers) {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
+public class ServersPool implements IServersPool {
+    private LinkedList<String> servers;
+    private ListIterator<String> iterator;
+    public ServersPool(String[] servers) {
+        this.servers = new LinkedList<String>(Arrays.asList(servers));
+        this.iterator = this.servers.listIterator();
     }
 
     public ServersPool(String serversFilePath) {
@@ -15,6 +22,12 @@ public class ServersPool implements IServersPool {
 
     @Override
     public String nextServer() {
-        return null;
+        if(servers.size() == 1) {
+            return servers.get(0);
+        }
+        if(!iterator.hasNext()) {
+            iterator = servers.listIterator();
+        }
+        return iterator.next();
     }
 }
