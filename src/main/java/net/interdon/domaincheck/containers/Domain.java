2df5ebd108589e4088e1104271e5b29f4c65d661
Evgeniy Dolgikh <kasarch@atsy.org.ua>
2013-12-23 18:29:58 +0200

    domain container

---
+++ src/main/java/net/interdon/domaincheck/containers/Domain.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2013 Evgeniy Dolgikh <marcon@atsy.org.ua>
+ * See the file LICENSE for copying permission.
+ */
+package net.interdon.domaincheck.containers;
+
+public class Domain {
+    private final String domainName;
+    private String dateCreated;
+    private String dateLastModifyed;
+    private String expirationDate;
+    private String whoisResponce;
+
+    public Domain(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDateLastModifyed() {
+        return dateLastModifyed;
+    }
+
+    public void setDateLastModifyed(String dateLastModifyed) {
+        this.dateLastModifyed = dateLastModifyed;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getWhoisResponce() {
+        return whoisResponce;
+    }
+
+    public void setWhoisResponce(String whoisResponce) {
+        this.whoisResponce = whoisResponce;
+    }
+}
