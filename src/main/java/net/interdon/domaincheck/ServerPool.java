ccdb9b813e7153755c529e32c9ad944ae830313a
Evgeniy Dolgikh <kasarch@atsy.org.ua>
2013-12-25 20:46:55 +0200

    server pool loading

--- src/main/java/net/interdon/domaincheck/ServerPool.java
+++ src/main/java/net/interdon/domaincheck/ServerPool.java
@@ -5,10 +5,21 @@
 
 package net.interdon.domaincheck;
 
+import net.interdon.domaincheck.parsers.ServerPoolConfigParser;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class ServerPool implements IServerPool {
-    public ServerPool(String pathToServerListXMLFile) {
+    private HashMap<String, String> servers;
+    public ServerPool(String pathToServerListXMLFile) throws ParserConfigurationException, SAXException, IOException,
+            XPathExpressionException {
+        servers = new HashMap<>();
+        loadServersList(pathToServerListXMLFile);
     }
 
     @Override
@@ -34,5 +45,17 @@ public class ServerPool implements IServerPool {
     @Override
     public List<String> tldList() {
         return null;
+    }
+
+    private void loadServersList(String xmlFilePath) throws ParserConfigurationException, SAXException, IOException,
+            XPathExpressionException{
+        ServerPoolConfigParser cfgParser = new ServerPoolConfigParser(xmlFilePath);
+        if(cfgParser.tldCount() > 0) {
+            List<String> tldList = cfgParser.getTldList();
+            for(String tld: tldList) {
+                servers.put(tld, cfgParser.getServer(tld));
+            }
+        }
+
     }
 }
