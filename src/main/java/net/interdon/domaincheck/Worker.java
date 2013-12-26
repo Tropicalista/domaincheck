4559c31d0e82a0c115bb986e17b9437e1aaf16c7
Evgeniy Dolgikh <marcon@atsy.org.ua>
2013-12-26 20:56:22 +0200

    put results in queue for parsing

--- src/main/java/net/interdon/domaincheck/Worker.java
+++ src/main/java/net/interdon/domaincheck/Worker.java
@@ -6,21 +6,24 @@ import org.apache.commons.net.whois.WhoisClient;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Queue;
 
 public class Worker extends Thread {
     private List<Domain> domains;
     private IServerPool serverPool;
     private int waitTime = 3000;
     private WhoisClient whoisClient;
+    private Queue<Domain> resultsQueue;
 
-    public Worker(List<Domain> domains, IServerPool serverPool) {
+    public Worker(List<Domain> domains, IServerPool serverPool, Queue<Domain> resultsQueue) {
         this.domains = domains;
         this.serverPool = serverPool;
+        this.resultsQueue = resultsQueue;
         whoisClient = new WhoisClient();
     }
 
-    public Worker(List<Domain> domains, IServerPool serverPool, int waitTime) {
-        this(domains, serverPool);
+    public Worker(List<Domain> domains, IServerPool serverPool, Queue<Domain> resultsQueue, int waitTime) {
+        this(domains, serverPool, resultsQueue);
         this.waitTime = waitTime;
     }
 
@@ -43,13 +46,14 @@ public class Worker extends Thread {
         } catch (NoServersForTldException e) {
             host = WhoisClient.DEFAULT_HOST;
         }
-        System.out.println(String.format("Querying host %s for domain %s", host, domain.getDomainName()));
-        /*try {
+        try {
             whoisClient.connect(WhoisClient.DEFAULT_HOST);
             domain.setWhoisResponce(whoisClient.query(domain.getDomainName()));
             whoisClient.disconnect();
         } catch (IOException e) {
-            this.interrupt();
-        }*/
+            domain.setWhoisResponce("");
+            return;
+        }
+        resultsQueue.add(domain);
     }
 }
