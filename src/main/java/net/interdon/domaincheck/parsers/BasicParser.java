1264d9953f58ca437d02372441a7b0ef60c1687e
Evgeniy Dolgikh <marcon@atsy.org.ua>
2013-12-26 21:02:03 +0200

    Basicparser implements IDomainParser interface

--- src/main/java/net/interdon/domaincheck/parsers/BasicParser.java
+++ src/main/java/net/interdon/domaincheck/parsers/BasicParser.java
@@ -10,24 +10,22 @@ import net.interdon.domaincheck.containers.Domain;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BasicParser {
+public class BasicParser implements IDomainParser {
     private Pattern expirationDatePattern;
 
     public BasicParser(String expirationDateRegexp) {
         expirationDatePattern = Pattern.compile(expirationDateRegexp);
     }
 
-    public Domain parse(Domain sourceDomain) {
+    public void parse(Domain sourceDomain) {
         if(sourceDomain.getWhoisResponce().isEmpty()) {
-            return sourceDomain;
+            return;
         }
         Matcher matcher = expirationDatePattern.matcher(sourceDomain.getWhoisResponce());
         if(matcher.find()) {
             String value = getMatchString(matcher, sourceDomain.getWhoisResponce());
             sourceDomain.setExpirationDate(value.split(":")[1].trim());
-            return sourceDomain;
         }
-        return sourceDomain;
     }
 
     private static String getMatchString(Matcher matcher, String source) {
