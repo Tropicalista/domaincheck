3d6051108aa3ff1ad3f5cee3ff8c75c8262bf2d6
Evgeniy Dolgikh <marcon@atsy.org.ua>
2013-12-26 21:14:07 +0200

    add domains processing

--- src/main/java/net/interdon/domaincheck/DomainCheck.java
+++ src/main/java/net/interdon/domaincheck/DomainCheck.java
@@ -6,17 +6,24 @@
 package net.interdon.domaincheck;
 
 import net.interdon.domaincheck.containers.Domain;
+import net.interdon.domaincheck.parsers.IDomainParser;
+import net.interdon.domaincheck.parsers.ParserFactory;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DomainCheck {
     private List<Domain> domainsToCheck;
+    private Queue<Domain> parsingQueue;
     private IServerPool servers;
 
     public DomainCheck(String[] domainsToCheck, IServerPool servers) {
         setServerPool(servers);
         buildDomainsList(domainsToCheck);
+        parsingQueue = new ConcurrentLinkedQueue<>();
     }
 
     public void setServerPool(IServerPool servers) {
@@ -30,4 +37,19 @@ public class DomainCheck {
         }
     }
 
+    public void start() {
+        Worker worker = new Worker(domainsToCheck, servers, parsingQueue);
+        ParserFactory factory = new ParserFactory();
+        HashMap<String, IDomainParser> parserPool = new HashMap<>();
+        Domain tmp;
+        while (!worker.isInterrupted()) {
+            if(!parsingQueue.isEmpty()) {
+                tmp = parsingQueue.poll();
+                if(!parserPool.containsKey(tmp.getTld())) {
+                    parserPool.put(tmp.getTld(), factory.newParser(tmp.getTld()));
+                }
+                parserPool.get(tmp.getTld()).parse(tmp);
+            }
+        }
+    }
 }
