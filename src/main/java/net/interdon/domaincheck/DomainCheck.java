d76c78b495cc425ece360c4e7ea8c9d567ef815b
Evgeniy Dolgikh <marcon@atsy.org.ua>
2013-12-26 19:42:11 +0200

    remove query method(querying will be done with workers)

--- src/main/java/net/interdon/domaincheck/DomainCheck.java
+++ src/main/java/net/interdon/domaincheck/DomainCheck.java
@@ -6,43 +6,21 @@
 package net.interdon.domaincheck;
 
 import net.interdon.domaincheck.containers.Domain;
-import net.interdon.domaincheck.exceptions.NoServersForTldException;
-import org.apache.commons.net.whois.WhoisClient;
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class DomainCheck {
     private List<Domain> domains;
     private IServerPool servers;
-    WhoisClient whoisClient;
 
     public DomainCheck(String[] domainsToCheck, IServerPool servers) {
         setServerPool(servers);
         buildDomainsList(domainsToCheck);
-        whoisClient = new WhoisClient();
     }
 
     public void setServerPool(IServerPool servers) {
         this.servers = servers;
-    }
-
-    public Domain query(Domain domain) {
-        String host;
-        try {
-            host = servers.nextServer(domain.getTld());
-        } catch (NoServersForTldException e) {
-            host = WhoisClient.DEFAULT_HOST;
-        }
-        try {
-            whoisClient.connect(WhoisClient.DEFAULT_HOST);
-            domain.setWhoisResponce(whoisClient.query(domain.getDomainName()));
-            whoisClient.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return domain;
     }
 
     private void buildDomainsList(String[] domainsToCheck) {
