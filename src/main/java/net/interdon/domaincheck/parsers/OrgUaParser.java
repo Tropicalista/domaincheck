51cd459a31b1acca55409b1222e2ea2988d94b99
Evgeniy Dolgikh <marcon@atsy.org.ua>
2013-12-31 12:39:17 +0200

    parse modification time and registration time

--- src/main/java/net/interdon/domaincheck/parsers/OrgUaParser.java
+++ src/main/java/net/interdon/domaincheck/parsers/OrgUaParser.java
@@ -11,7 +11,11 @@ import java.util.regex.Pattern;
 
 public class OrgUaParser extends AbstractParser {
     private static final String EXPIRATION_DATE_REGEXP = "\\bexpires\\b:{1}\\s*\\d{4}-{1}\\d{2}-{1}\\d{2}"; //matches "expires: YYYY-MM-DD"
+    private static final String MODIFIED_DATE_REGEXP = "\\bmodified\\b:{1}\\s*\\d{4}-{1}\\d{2}-{1}\\d{2}";
+    private static final String CREATED_DATE_REGEXP = "\\bmodified\\b:{1}\\s*\\d{4}-{1}\\d{2}-{1}\\d{2}";
     private static final Pattern expirationDatePattern = Pattern.compile(EXPIRATION_DATE_REGEXP);
+    private static final Pattern modifiedDatePattern = Pattern.compile(MODIFIED_DATE_REGEXP);
+    private static final Pattern createdDatePattern = Pattern.compile(CREATED_DATE_REGEXP);
 
     @Override
     public void parse(Domain sourceDomain) {
@@ -21,6 +25,14 @@ public class OrgUaParser extends AbstractParser {
         String value = getFieldValue(getMatchField(expirationDatePattern, sourceDomain.getWhoisResponce()), ":");
         if(!value.isEmpty()) {
             sourceDomain.setExpirationDate(value);
+        }
+        value = getFieldValue(getMatchField(modifiedDatePattern, sourceDomain.getWhoisResponce()), ":");
+        if(!value.isEmpty()) {
+            sourceDomain.setDateLastModifyed(value);
+        }
+        value = getFieldValue(getMatchField(createdDatePattern, sourceDomain.getWhoisResponce()), ":");
+        if(!value.isEmpty()) {
+            sourceDomain.setDateCreated(value);
         }
 
     }
