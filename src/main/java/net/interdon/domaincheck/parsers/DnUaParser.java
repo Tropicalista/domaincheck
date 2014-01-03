852db89043600739cd44baf55ebadfc1026b2686
Evgeniy Dolgikh <marcon@atsy.org.ua>
2014-01-03 19:28:50 +0200

    update regexp

--- src/main/java/net/interdon/domaincheck/parsers/DnUaParser.java
+++ src/main/java/net/interdon/domaincheck/parsers/DnUaParser.java
@@ -5,9 +5,9 @@ import net.interdon.domaincheck.containers.Domain;
 import java.util.regex.Pattern;
 
 public class DnUaParser extends AbstractParser {
-    private static final String EXPIRATION_DATE_REGEXP = "\\bexpires\\b:{1}\\s*\\d{4}-{1}\\d{2}-{1}\\d{2}"; //matches "expires: YYYY-MM-DD"
-    private static final String MODIFIED_DATE_REGEXP = "\\bupdated:\\b:{1}\\s*\\d{4}-{1}\\d{2}-{1}\\d{2}";
-    private static final String CREATED_DATE_REGEXP = "\\bcreated:\\b:{1}\\s*\\d{4}-{1}\\d{2}-{1}\\d{2}";
+    private static final String EXPIRATION_DATE_REGEXP = "\\bexpires\\b:\\s*\\d{4}-\\d{2}-\\d{2}"; //matches "expires: YYYY-MM-DD"
+    private static final String MODIFIED_DATE_REGEXP = "\\bupdated\\b:\\s*\\d{4}-\\d{2}-\\d{2}";
+    private static final String CREATED_DATE_REGEXP = "\\bregistered\\b:\\s*\\d{4}-\\d{2}-\\d{2}";
     private static final Pattern expirationDatePattern = Pattern.compile(EXPIRATION_DATE_REGEXP);
     private static final Pattern modifiedDatePattern = Pattern.compile(MODIFIED_DATE_REGEXP);
     private static final Pattern createdDatePattern = Pattern.compile(CREATED_DATE_REGEXP);
