671dc16273c58ae9ba6606b39cd0cc001cb38fb5
Evgeniy Dolgikh <marcon@atsy.org.ua>
2014-01-03 22:12:56 +0200

    add month enum

---
+++ src/main/java/net/interdon/domaincheck/containers/Month.java
@@ -0,0 +1,16 @@
+package net.interdon.domaincheck.containers;
+
+public enum Month {
+    JAN,
+    FEB,
+    MAR,
+    APR,
+    MAY,
+    JUN,
+    JUL,
+    AUG,
+    SEP,
+    OCT,
+    NOV,
+    DEC
+}
